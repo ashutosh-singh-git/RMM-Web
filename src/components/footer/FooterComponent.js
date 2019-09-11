faca65765fca550a4597615d7e89fdf1145a2f30
Ashutosh Singh <ashutosh.singh5335@gmail.com>
2019-09-11 20:31:25 +0530

    Fixed Footer Css + Text Change

--- src/components/footer/FooterComponent.js
+++ src/components/footer/FooterComponent.js
@@ -10,7 +10,7 @@ class FooterComponent extends Component {
                     <div className="row text-white">
                         <div className="col-lg-8">
                             <h6>Rate My Manager</h6>
-                            <p className="small">Write to us at risingofminds@gmail.com<br/>
+                            <p className="">Write to us at risingofminds@gmail.com<br/>
                                 Plot-62, Sector - 23, Gurugram, 110092<br/>
                                 +91-8285932260
                             </p>
@@ -18,7 +18,7 @@ class FooterComponent extends Component {
                         <div className="col-lg-4">
                             <div className="float-left">
                                 <h6>Navigation</h6>
-                                <p className="small">
+                                <p className="">
                                     <Link to="/">Home</Link><br/>
                                     <Link to="/add">Add Manager</Link><br/>
                                     <a target="blank" href={"https://docs.google.com/forms/d/1ol15p1pkaXPOr4B3IPONE4AqY9nprTYXdFhkcTfRUeg/prefill"}>Feedback </a><br/>
@@ -27,7 +27,7 @@ class FooterComponent extends Component {
                             </div>
                             <div className="float-right">
                                 <h6>Follow Us On</h6>
-                                <p className="small">
+                                <p className="">
                                     Facebook<br/>
                                     Twitter <br/>
                                     LinkedIn<br/>
